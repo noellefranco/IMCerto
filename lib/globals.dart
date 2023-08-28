873fd519000cbdaaac7779dfe465558735edc842
noelle <noelle.mtf@gmail.com>
2023-08-28 03:03:11 -0300

    FEAT: add buttons for weight and age

--- lib/globals.dart
+++ lib/globals.dart
@@ -3,16 +3,16 @@ import 'package:flutter/material.dart';
 const kDefaultTextStyle = TextStyle(
   fontSize: 20.0,
   fontWeight: FontWeight.w700,
-  color: Color(0xFF5f4d3c),
+  color: Color(0xFF23282D),
 );
 const kDfaultSzedBoxHeight = 15.0;
 const kDefaultIconHeight = 95.0;
 const kDefaultIconColor = Color(0xffFAF7F5);
 const kActiveCardDefaultColor = Color(0xffed6310);
-const kFoterColor = Color(0xFF5f4d3c);
 const kInactiveCardDefaultColor = Color(0xffEEE4DE);
-const kHeightNumTextStyle = TextStyle(
+const kNumTextStyle = TextStyle(
   fontSize: 50.0,
   fontWeight: FontWeight.w900,
   color: Color(0xffFAF7F5),
 );
+const kCalculateFooter = Color(0xff107eed);
--- lib/home.dart
+++ lib/home.dart
@@ -21,6 +21,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   Sex? selectedGender;
   int userHeight = 180;
+  int userWeight = 70;
+  int userAge = 18;
 
   @override
   Widget build(BuildContext context) {
@@ -84,7 +86,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     crossAxisAlignment: CrossAxisAlignment.baseline,
                     textBaseline: TextBaseline.alphabetic,
                     children: [
-                      Text(userHeight.toString(), style: kHeightNumTextStyle),
+                      Text(userHeight.toString(), style: kNumTextStyle),
                       Text(
                         'cm'.toLowerCase(),
                         style: kDefaultTextStyle,
@@ -92,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     ],
                   ),
                   Slider(
-                    activeColor: Color(0xff107eed),
+                    activeColor: kCalculateFooter,
                     inactiveColor: Color(0xffeee4de),
                     onChanged: (double newValue) {
                       setState(() {
@@ -111,24 +113,124 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Row(
               children: [
                 Expanded(
-                  child:
-                      DefaultContainer(selectedColor: kActiveCardDefaultColor),
+                  child: DefaultContainer(
+                    selectedColor: kActiveCardDefaultColor,
+                    childCard: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'PESO (kg)',
+                          style: kDefaultTextStyle,
+                        ),
+                        Text(
+                          userWeight.toString(),
+                          style: kNumTextStyle,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            RoundedButton(
+                              icon: FontAwesomeIcons.minus,
+                              handlePressed: () {
+                                setState(() {
+                                  userWeight--;
+                                });
+                              },
+                            ),
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            RoundedButton(
+                              icon: FontAwesomeIcons.plus,
+                              handlePressed: () {
+                                setState(() {
+                                  userWeight++;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
                 Expanded(
-                  child:
-                      DefaultContainer(selectedColor: kActiveCardDefaultColor),
+                  child: DefaultContainer(
+                    selectedColor: kActiveCardDefaultColor,
+                    childCard: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'idade'.toUpperCase(),
+                          style: kDefaultTextStyle,
+                        ),
+                        Text(
+                          userAge.toString(),
+                          style: kNumTextStyle,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            RoundedButton(
+                              icon: FontAwesomeIcons.minus,
+                              handlePressed: () {
+                                setState(() {
+                                  userAge--;
+                                });
+                              },
+                            ),
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            RoundedButton(
+                              icon: FontAwesomeIcons.plus,
+                              handlePressed: () {
+                                setState(() {
+                                  userAge++;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ],
             ),
           ),
           Container(
-            color: kFoterColor,
+            color: kCalculateFooter,
             margin: const EdgeInsets.only(top: 10.0),
             width: double.infinity,
             height: 80.0,
           )
         ],
       ),
+    );
+  }
+}
+
+class RoundedButton extends StatelessWidget {
+  RoundedButton({required this.icon, required this.handlePressed});
+
+  final IconData icon;
+  final Function handlePressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      onPressed: () {
+        handlePressed();
+      },
+      elevation: 6.0,
+      constraints: BoxConstraints.tightFor(
+        width: 56.0,
+        height: 56.0,
+      ),
+      shape: CircleBorder(),
+      fillColor: Color(0xffd34500),
+      child: Icon(icon),
     );
   }
 }
